4b81b055b5922f919331eded5ec341fabafeb4e2
Marko <neonmarko2@gmail.com>
2026-03-28 02:31:27 +0100

    Remove Omni Whistle

--- src/main/java/net/neonmarko2/skeletonmessenger/item/ModItems.java
+++ src/main/java/net/neonmarko2/skeletonmessenger/item/ModItems.java
@@ -11,7 +11,6 @@ import net.neonmarko2.skeletonmessenger.item.custom.SkeletonWhistleItem;
 
 public class ModItems {
     public static final Item SKELETON_WHISTLE = registerItem("skeleton_whistle", new SkeletonWhistleItem(new Item.Settings().maxCount(1)));
-    public static final Item SKELETON_WHISTLE_OMNI = registerItem("skeleton_whistle_omni", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(SkeletonMessenger.MOD_ID, name), item);
@@ -22,7 +21,6 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(SKELETON_WHISTLE);
-            fabricItemGroupEntries.add(SKELETON_WHISTLE_OMNI);
         });
     }
 }
