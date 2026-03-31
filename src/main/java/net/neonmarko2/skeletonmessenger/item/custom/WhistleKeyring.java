69980a7da38816110615bcd7a6e330cf27ebbaa5
Marko <neonmarko2@gmail.com>
2026-03-31 21:43:06 +0200

    Add inventory to Keyring Both a temporary non saving inventory and a gui to enter the inventory (on use)

--- src/main/java/net/neonmarko2/skeletonmessenger/item/custom/WhistleKeyring.java
+++ src/main/java/net/neonmarko2/skeletonmessenger/item/custom/WhistleKeyring.java
@@ -1,16 +1,20 @@
 package net.neonmarko2.skeletonmessenger.item.custom;
 
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.screen.GenericContainerScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.world.World;
 
-public class WhistleKeyring extends Item {
+public class WhistleKeyring extends Item implements Inventory {
 
-    public DefaultedList<ItemStack> inventory = DefaultedList.ofSize(9, ItemStack.EMPTY);
+    public DefaultedList<ItemStack> inventory = DefaultedList.ofSize(18, ItemStack.EMPTY);
 
     public WhistleKeyring(Settings settings) {
         super(settings);
@@ -18,6 +22,63 @@ public class WhistleKeyring extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        return super.use(world, user, hand);
+        user.openHandledScreen(new SimpleNamedScreenHandlerFactory(
+                (syncId, playerInventory, player) ->
+                        new GenericContainerScreenHandler(ScreenHandlerType.GENERIC_9X2, syncId, playerInventory, this, 2),
+                getName()
+                ));
+
+        return TypedActionResult.success(user.getStackInHand(hand), false);
+    }
+
+
+
+    @Override
+    public int size() {
+        return 18;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public ItemStack getStack(int slot) {
+        return inventory.get(slot);
+    }
+
+    @Override
+    public ItemStack removeStack(int slot, int amount) {
+        return inventory.set(slot, inventory.get(slot));
+    }
+
+    @Override
+    public ItemStack removeStack(int slot) {
+        return inventory.remove(slot);
+    }
+
+    @Override
+    public void setStack(int slot, ItemStack stack) {
+        inventory.set(slot, stack);
+    }
+
+    @Override
+    public boolean isValid(int slot, ItemStack stack) {
+        return false;
+    }
+
+    @Override
+    public void markDirty() {
+    }
+
+    @Override
+    public boolean canPlayerUse(PlayerEntity player) {
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        inventory = DefaultedList.ofSize(27, ItemStack.EMPTY);
     }
 }
